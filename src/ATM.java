131ce5318bfa1aedd4b7795d9654517feff404df
Thomas Kressman <thomas.kressman@gmail.com>
2022-04-13 13:04:54 -0400

    Add ATM and Account class. Add start function to ATM

---
+++ src/ATM.java
@@ -0,0 +1,13 @@
+public class ATM {
+
+    public static void main(String[] args) {
+        start();
+    }
+
+    public static void start() {
+        System.out.println("Good day! Please enter you account details to get started.");
+        // Implement Login
+        // Implement Menu after logging in
+    }
+
+}
