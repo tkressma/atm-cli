9b8e048e2baaf1706563fb811d5aaba986ee60a6
Thomas Kressman <thomas.kressman@gmail.com>
2022-04-13 13:17:26 -0400

    Add accounts ArrayList to retrieve all the accounts in the database

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -2,8 +2,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public class CSVReader {
+    static ArrayList<Account> accounts = new ArrayList<>();
     File database = new File("User_Database");
 
     public CSVReader() {
@@ -18,6 +20,8 @@ public class CSVReader {
                 int acctId = Integer.parseInt(accountDetails[1]);
                 int acctPin = Integer.parseInt(accountDetails[2]);
                 BigDecimal acctBalance = new BigDecimal(accountDetails[3]);
+
+                accounts.add(new Account(acctName, acctId, acctPin, acctBalance));
             }
         } catch (Exception e) {
             System.out.println("Error loading file.");
