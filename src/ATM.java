13e2497903384b687ee3ccea6ce56ea62ae375f9
Thomas Kressman <thomas.kressman@gmail.com>
2022-04-13 13:43:43 -0400

    Add a way to retrieve input from user for login

--- src/ATM.java
+++ src/ATM.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class ATM {
     private static AccountDatabase accountDatabase = new AccountDatabase();
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         start();
@@ -11,18 +14,24 @@ public class ATM {
     }
 
     public static void login() {
-        int acctId = 55555555;
-        int acctPin = 1234;
+        int acctId = getLoginInput(8);
+        int acctPin = getLoginInput(4);
 
         while (!accountDatabase.authenticateUser(acctId, acctPin)) {
             System.out.println("\nInvalid Credentials. Please re-enter your account ID and Pin.\n");
-            // Implement a way for users to enter input and validate that input
-            break;
+            acctId = getLoginInput(8);
+            acctPin = getLoginInput(4);
         }
+        
     }
 
-    public static void getLoginInput() {
+    public static int getLoginInput(int inputLength) {
+        String inputType = inputLength == 8 ? "ID" : "PIN";
 
+        System.out.printf("Enter your %s: ", inputType);
+        String userInput = scanner.nextLine();
+
+        return Integer.parseInt(userInput);
     }
 
 
