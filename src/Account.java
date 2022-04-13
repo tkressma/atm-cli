025e9a5b7ce88b659163668ef487f21daefbdf0b
Thomas Kressman <thomas.kressman@gmail.com>
2022-04-13 13:08:09 -0400

    Add account constructor to Account class.

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,15 @@
+import java.math.BigDecimal;
+
 public class Account {
+    String name;
+    int id;
+    int pin;
+    BigDecimal balance;
+
+    public Account(String acctName, int acctId, int acctPin, BigDecimal acctBalance) {
+        this.name = acctName;
+        this.id = acctId;
+        this.pin = acctPin;
+        this.balance = acctBalance;
+    }
 }
