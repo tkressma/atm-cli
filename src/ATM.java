25540cdc787bbf2ff65c50c4e126a664d69c5aa8
Thomas Kressman <thomas.kressman@gmail.com>
2022-04-14 12:33:24 -0400

    Add constant variables for id and pin input length

--- src/ATM.java
+++ src/ATM.java
@@ -7,6 +7,8 @@ public class ATM {
     private static AccountDatabase accountDatabase = new AccountDatabase();
     private static Scanner scanner = new Scanner(System.in);
     private static Account currentUser;
+    static final int PIN_LENGTH = 8;
+    static final int ID_LENGTH = 4;
 
     public static void main(String[] args) {
         start();
@@ -20,20 +22,20 @@ public class ATM {
     }
 
     public static void login() {
-        int accountId = getLoginInput(8);
-        int accountPin = getLoginInput(4);
+        int accountId = getLoginInput(ID_LENGTH);
+        int accountPin = getLoginInput(PIN_LENGTH);
 
         while (!accountDatabase.authenticateUser(accountId, accountPin)) {
             System.out.println("\nInvalid Credentials. Please re-enter your account ID and Pin.\n");
-            accountId = getLoginInput(8);
-            accountPin = getLoginInput(4);
+            accountId = getLoginInput(ID_LENGTH);
+            accountPin = getLoginInput(PIN_LENGTH);
         }
         currentUser = accountDatabase.getAccount(accountId);
     }
 
     // Retrieves account ID/PIN based on the input length provided (8 for ID, 4 for PIN).
     public static int getLoginInput(int inputLength) {
-        String inputType = inputLength == 8 ? "ID" : "PIN";
+        String inputType = inputLength == ID_LENGTH ? "ID" : "PIN";
 
         System.out.printf("Enter your %s: ", inputType);
         ;
@@ -187,14 +189,14 @@ public class ATM {
     // Re-Authenticate the user if returning to the menu to perform more actions.
     public static void userReAuthentication() {
         System.out.println("\nPlease re-enter your PIN to continue:");
-        int accountPin = getLoginInput(4);
+        int accountPin = getLoginInput(PIN_LENGTH);
         int attempts = 0;
 
         while (accountPin != currentUser.accountPin) {
             attempts++;
             if (attempts == 3) logout();
             System.out.println("\nIncorrect PIN. Please re-enter your pin to continue:");
-            accountPin = getLoginInput(4);
+            accountPin = getLoginInput(PIN_LENGTH);
         }
 
         mainMenu();
